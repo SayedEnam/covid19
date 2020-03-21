1f0619d77027a9f0e0207d59aff223d4e17dba73
SayedEnam <syedenamshohag@gmail.com>
2020-03-21 23:38:55 +0600

    Updates

---
+++ precache-manifest.3003908e7ad83655191b032bde129967.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07aca9e193d3b6cb4b2dac29ba9db758",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "5db341f3ae9fd3b9ca05",
+    "url": "/covid19/static/css/main.10168593.chunk.css"
+  },
+  {
+    "revision": "40690dad87b99efd3602",
+    "url": "/covid19/static/js/2.4140f098.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid19/static/js/2.4140f098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5db341f3ae9fd3b9ca05",
+    "url": "/covid19/static/js/main.2140c344.chunk.js"
+  },
+  {
+    "revision": "2897f32d4e7477ba260b",
+    "url": "/covid19/static/js/runtime-main.a021e852.js"
+  }
+]);
